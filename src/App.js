ceadb5624b45bddc9298e83b4729e72f23f6cd8a
staffanholmnas <staffan.holmnas@centria.fi>
2022-01-04 23:36:17 +0200

    Edit app for chapter 4

--- src/App.js
+++ src/App.js
@@ -34,4 +34,20 @@ function App(props) {
   );
 }
 
+function App2() {
+  const [count, setCount] = useState(0);
+  
+  return (
+    <div className="App">
+      {count <= 10 
+        ? <p>You have pressed the button {count} times</p> 
+        : <p>You have pressed the button more than 10 times</p>
+      }
+      <button onClick={()=> setCount(count + 1)}>+</button>
+      <button onClick={()=> setCount(count - 1)}>-</button>
+      <button onClick={()=> setCount(0)}>Clear</button>
+    </div>
+  );
+}
+
 export default MyComponent;
