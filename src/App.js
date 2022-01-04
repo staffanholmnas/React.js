ade2fb2a30ad02d034485933434a0457cd9bf6c8
staffanholmnas <staffan.holmnas@centria.fi>
2022-01-04 21:10:06 +0200

    ch 3 open exercise 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
 
-
 function App() {
   return (
     <div className='App'>
-       <p style={{fontSize: '50px'}}>Hello World!</p>
-       <button>press me</button>
-      <p className='Alert'>Lorum ipsum</p>
+      <h1 className='Welcome'>Welcome to my site</h1> 
+      <p className='First'>This is my first React app</p>
     </div>
   );
 }
-
 
 export default App;
