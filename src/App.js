c3717d3e3f5efdafc87a100aa4fb1b8f7fd0e1af
staffanholmnas <staffan.holmnas@centria.fi>
2022-01-05 15:35:14 +0200

    add form app

--- src/App.js
+++ src/App.js
@@ -50,4 +50,32 @@ function App2() {
   );
 }
 
+function FormApp() {
+  const [person, setPerson] = useState({firstname: '', lastname: '', email: ''});
+
+  const inputChanged = (event) => {
+    setPerson({...person, [event.target.name]: event.target.value});
+  }
+
+  const showAlert = () => {
+    alert(`Hello ${person.firstname} ${person.lastname}`);
+  }
+
+  const formSubmitted = (event) => {
+    event.preventDefault();
+     showAlert();
+    }
+
+  return (
+  <div className="App">
+      Name: {person.firstname} {person.lastname} Email: {person.email}<br />
+      <form onSubmit={formSubmitted}>
+    <input placeholder="First name" name="firstname" value={person.firstname} onChange={inputChanged} />
+    <input placeholder="Last name" name="lastname" value={person.lastname} onChange={inputChanged} />
+    <input placeholder="Email" name="email" value={person.email} onChange={inputChanged} />
+    <input type="submit" value="Submit" />
+    </form>
+  </div>
+  );
+}
 export default MyComponent;
