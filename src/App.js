4dc5326fa35752f513b13855ee6195a23cd189f0
staffanholmnas <staffan.holmnas@centria.fi>
2022-01-06 22:45:54 +0200

    add user app

--- src/App.js
+++ src/App.js
@@ -56,5 +56,33 @@ function UsersApp() {
   );
 }
 
+function UserApp() {
+  const [user, setUser] = useState({});
+  const [userid, setUserid] = useState('')
+
+  const fetchData = () => {
+    fetch('https://reqres.in/api/users/' + userid)
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error('Response status not ok');
+      }
+
+      return response.json();
+    })
+    .then(resData => setUser(resData.data))
+  };
+
+  const inputChanged = (event) => {
+    setUserid(event.target.value);
+  }
+
+  return (
+    <div className="App">
+      <input placeholder="User ID" value={userid} onChange={inputChanged} />
+      <button onClick={fetchData}>Fetch</button>
+      <p>{user.first_name} {user.last_name} {user.email}</p>
+    </div>
+  );
+}
 
 export default UsersApp;
