d1101e969fa235f22ecf8bf3e6be9f4d0df42045
staffanholmnas <staffan.holmnas@centria.fi>
2022-01-07 11:41:38 +0200

    Add github api app

--- src/App2.js
+++ src/App2.js
@@ -1,119 +1,110 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function StateApp() {
-  const [firstName, setFirstName] = useState('John');
-  const [message, setMessage] = useState('Hello')
-  const [person, setPerson] = useState({firstName: 'Ben', lastName: 'Kenobi', age: 50});
-  const [count, setCount] = useState(30);
-  
-  return(
-    
-    <div>
-      {message} {firstName} {count}
-      <p>{person.firstName} {person.lastName} {person.age}</p>
-        <button onClick={()=> {
-          setPerson({firstName: 'Luke', lastName: 'Skywalker', age: 30});
-          setFirstName('Anakin')}}>
-          Use the force!
-        </button>
-        <button onClick={()=> {
-          setPerson({...person, age: 25});
-          setCount(count + 1)}}>
-          Grow older!
-        </button>
-      </div>
-  )
-}
+function NasaApp() {
+  const [explanation, setExplanation] = useState('');
+  const [url, setUrl] = useState('');
+  const [isReady, setReady] = useState(false);
 
-function PropsApp(props) {
+  useEffect(() => {
+    fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(response => response.json())
+    .then(data => {
+      setExplanation(data.explanation)
+      setUrl(data.url)
+      setReady(true);
+    })
+    .catch(err => console.log(err))
+  },[])
+
+  if (!isReady)
+    return <div>Loading...</div>
+
   return (
-    <div className='App'>
-      Hello World {props.firstname} {props.lastname}
+    <div className="App">
+      <p>{explanation}</p>
+      <img alt="Nasa apod" src={url} />
     </div>
   );
 }
 
-function CountApp() {
-  const [count, setCount] = useState(0);
-  
-  return (
-    <div className="App">
-      {count <= 10 
-        ? <p>You have pressed the button {count} times</p> 
-        : <p>You have pressed the button more than 10 times</p>
-      }
-      <button onClick={()=> setCount(count + 1)}>+</button>
-      <button onClick={()=> setCount(count - 1)}>-</button>
-      <button onClick={()=> setCount(0)}>Clear</button>
-    </div>
-  );
-}
+function UsersApp() {
+  const [users, setUsers] = useState([]);
 
-function FormApp() {
-  const [person, setPerson] = useState({firstname: '', lastname: '', email: ''});
-
-  const inputChanged = (event) => {
-    setPerson({...person, [event.target.name]: event.target.value});
-  }
-
-  const showAlert = () => {
-    alert(`Hello ${person.firstname} ${person.lastname}`);
-  }
-
-  const formSubmitted = (event) => {
-    event.preventDefault();
-     showAlert();
-    }
-
-  return (
-  <div className="App">
-      Name: {person.firstname} {person.lastname} Email: {person.email}<br />
-      <form onSubmit={formSubmitted}>
-    <input placeholder="First name" name="firstname" value={person.firstname} onChange={inputChanged} />
-    <input placeholder="Last name" name="lastname" value={person.lastname} onChange={inputChanged} />
-    <input placeholder="Email" name="email" value={person.email} onChange={inputChanged} />
-    <input type="submit" value="Submit" />
-    </form>
-  </div>
-  );
-}
-
-function TodoApp() {
-  const [todo, setTodo] = useState({description: '', date: ''});
-  const [todos, setTodos] = useState([]);
-
-  const inputChanged = (event) => {
-    setTodo({...todo, [event.target.name]: event.target.value});
-  }
-
-  const addTodo = () => {
-    setTodos([...todos, todo]);
-    setTodo({description: '', date: ''});
-  }
-
-  const deleteTodo = (row) => {
-    setTodos(todos.filter((todo,index) => index !== row));
-  }
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+    .then(response => response.json())
+    .then(resData => setUsers(resData.data))
+  }, []);
 
   return (
     <div className="App">
-      <input placeholder="Description" name="description" value={todo.description} onChange={inputChanged} />
-      <input placeholder="Date" name="date" value={todo.date} onChange={inputChanged} />
-      <button onClick={addTodo}>Add</button>
-      <table style={{margin: 'auto'}}>
-        <tbody style={{textAlign: 'left'}}>
-          { 
-            todos.map((todo, index) =>
-            <tr key={index}>
-              <td>{todo.description}</td>
-              <td>{todo.date}</td>
-              <td><button onClick={() => deleteTodo(index)}>Delete</button></td>
-            </tr>) 
-          }
+      <table>
+        <tbody>
+        {
+          users.map((user, index) => 
+           <tr key={index}>
+             <td>{user.first_name}</td>
+             <td>{user.last_name}</td>
+             <td>{user.email}</td>
+             <td><p><img src={user.avatar} alt='user avatar'></img></p></td>
+           </tr>)
+        }
         </tbody>
       </table>
     </div>
   );
 }
-export default TodoApp;
+
+function UserApp() {
+  const [user, setUser] = useState({});
+  const [userid, setUserid] = useState('')
+
+  const fetchData = () => {
+    fetch('https://reqres.in/api/users/' + userid)
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error('Response status not ok');
+      }
+
+      return response.json();
+    })
+    .then(resData => setUser(resData.data))
+  };
+
+  const inputChanged = (event) => {
+    setUserid(event.target.value);
+  }
+
+  return (
+    <div className="App">
+      <input placeholder="User ID" value={userid} onChange={inputChanged} />
+      <button onClick={fetchData}>Fetch</button>
+      <p>{user.first_name} {user.last_name} {user.email}</p>
+    </div>
+  );
+}
+
+function TriviaApp() {
+  const [trivia, setTrivia] = useState({});
+  
+  const fetchData = () => {
+    fetch('https://opentdb.com/api.php?amount=1')
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error('Response status not ok');
+      }
+      return response.json();
+    })
+    .then(data => setTrivia(data.results[0]))
+  };
+
+  return (
+    <div className="App">
+      <p>{trivia.question}</p>
+      <button onClick={fetchData}>New question</button>
+    </div>
+  );
+}
+
+export default UsersApp;
