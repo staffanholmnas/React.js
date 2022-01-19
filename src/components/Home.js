6c1fef2f534db605eb4ba0bce9771cd7fb3fb40b
staffanholmnas <staffan.holmnas@centria.fi>
2022-01-19 15:54:09 +0200

    add carousel, fix bugs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
+import Carousel from 'react-bootstrap/Carousel';
 
 function Home() {
   return (
@@ -7,6 +8,33 @@ function Home() {
       <br></br>
       <Typography variant="h6">
         This is a collection of apps made with React, enjoy!
+        <p></p>
+        <br></br>
+        <div className='carousel'>
+          <Carousel variant='dark'>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={require("../pics/pic4.jpg")}
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={require("../pics/pic5.jpg")}
+                alt="Second slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={require("../pics/pic7.jpg")}
+                alt="Third slide"
+              />
+            </Carousel.Item>
+          </Carousel>
+        </div>
       </Typography>
     </div>
   )
--- src/components/Nasa.js
+++ src/components/Nasa.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -10,14 +10,14 @@ function Nasa() {
 
   useEffect(() => {
     fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-    .then(response => response.json())
-    .then(data => {
-      setExplanation(data.explanation)
-      setUrl(data.url)
-      setReady(true);
-    })
-    .catch(err => console.log(err))
-  },[])
+      .then(response => response.json())
+      .then(data => {
+        setExplanation(data.explanation)
+        setUrl(data.url)
+        setReady(true);
+      })
+      .catch(err => console.log(err))
+  }, [])
 
   if (!isReady)
     return <div><br></br><br></br>Loading...</div>
@@ -33,9 +33,13 @@ function Nasa() {
       </AppBar>
       <p></p>
       <br></br>
-      <h4>Nasa's picture of the day</h4>
-      <p>{explanation}</p>
-      <img alt="Nasa apod" src={url} />
+      <div className='nasatext'>
+        <h4>Nasa's picture of the day</h4>
+        <p>{explanation}</p>
+      </div>
+      <div >
+        <img alt="Nasa apod" src={url} className='nasapic' />
+      </div>
     </div>
   );
 }
